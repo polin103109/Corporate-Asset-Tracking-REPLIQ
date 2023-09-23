3686a19c8c151455b7f5a0a2a1f1d4e7efca1f11
polin103109 <sabrinpolinece18@gmail.com>
2023-09-23 14:58:51 +0600

    create views for serializers

--- assettracking/views.py
+++ assettracking/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+#rom django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .models import Company, Employee, Device, Allocation
+from .serializers import CompanySerializer, EmployeeSerializer, DeviceSerializer, AllocationSerializer
+
+class CompanyListCreateView(generics.ListCreateAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+class EmployeeListCreateView(generics.ListCreateAPIView):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+
+class DeviceListCreateView(generics.ListCreateAPIView):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
+
+class AllocationListCreateView(generics.ListCreateAPIView):
+    queryset = Allocation.objects.all()
+    serializer_class = AllocationSerializer
+
